123954327e0a3c1aab721e892ed0426c67654703
Chris Gradwohl <christophergradwohl@gmail.com>
2017-04-15 12:40:24 -0700

    new dir structure

---
+++ server/config/env.js
@@ -0,0 +1,16 @@
+var path = require('path'),
+	rootPath = path.normalize(__dirname + '/../../');
+
+module.exports = {
+	development: {
+		rootPath: rootPath,
+		db: 'mongodb://localhost/mean-starter',
+		port: process.env.PORT || 3000
+	},
+	production: {
+		rootPath: rootPath,
+        db: 'mongodb://admin:123admin@ds161400.mlab.com:61400/sluggit-db',
+		// db: process.env.MONGOLAB_URI || 'you can add a mongolab uri here ($ heroku config | grep MONGOLAB_URI)',
+		port: process.env.PORT || 80
+	}
+};
