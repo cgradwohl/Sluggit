b6ba8c45c6ab84cea59cf78536c06d67b9bdb10e
Chris Gradwohl <christophergradwohl@gmail.com>
2017-04-20 16:59:34 -0700

    added passport config for user auth with JWT

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const express = require('express'),
 
 module.exports = function(app){
 
-	// angularjs catch all route
+	// Angularjs catches all routes
 	router.get('/*', function(req, res) {
 		res.sendFile(rootPath + 'dist/index.html', { user: req.user });
 	});
--- server.js
+++ server.js
@@ -5,18 +5,23 @@ const express = require('express'),
 	cookieParser = require('cookie-parser'),
     methodOverride = require('method-override'),
 	cors = require('cors'),
-	app = express();
+	passport = require('passport');
+
+    
 
 // ENVIRONMENT CONFIG
-const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-	envConfig = require('./config/environment')[env];
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const envConfig = require('./config/environment')[env];
 
 
-// connect to DB mabey add this to config
+// CONNECT TO DB
+mongoose.Promise = global.Promise;
 mongoose.connect(envConfig.db);
 
 
 // EXPRESS CONFIG
+const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -24,16 +29,28 @@ app.use(methodOverride());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
+
+// PASSPORT CONFIG
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+
 // ROUTES
-const users = require('./routes/users');
-const posts = require('./routes/posts');
-
-app.use('/users', users);
-app.use('/feed', posts);
-
+// sets server to serve dist folder, which which is built on the server
 require('./routes/index')(app);
 
-// Start server
+
+// API ROUTES
+const users = require('./api/users');
+app.use('/users', users);
+
+// const posts = require('./routes/posts');
+// app.use('/feed', posts);
+
+
+
+// START SERVER
 app.listen(envConfig.port, function(){
   console.log('Server listening on port ' + envConfig.port)
 });
